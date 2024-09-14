13a6ff2cd15d004f1677e7c68939319f2666028d
Ali Ilyes MANSOUR <mansour.aliilyes@gmail.com>
2024-09-14 14:04:52 +0100

    Fixed typo in likes table column like

--- database/migrations/2024_09_13_203026_create_likes_table.php
+++ database/migrations/2024_09_13_203026_create_likes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('video_id')->constrained()->onDelete('cascade');
-            $table->boolean('liek');
+            $table->boolean('like');
             $table->timestamps();
         });
     }
